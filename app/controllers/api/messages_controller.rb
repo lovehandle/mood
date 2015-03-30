e9cdd325d760f19d24d40229b0dd900cb55ac6d3
Ryan Closner <ryan@ryanclosner.com>
2015-03-29 23:08:54 -0400

    Fix messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,15 @@
 class Api::MessagesController < Api::ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   def handle
-    @message = Message.create(from: params["From"], to: params["To"], body: params["Body"], status: :sent)
+    @message = Message.create(
+      from: params["From"],
+      to: params["To"],
+      body: params["Body"],
+      remote_id: params["MessageSid"],
+      status: :sent
+    )
   end
 
 end
--- app/views/phones/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @phone, :id, :created_at, :updated_at
